1db725f337048aeb2ac5b08b89ea0b77458d5fbb
Artur Melo Favero <83616011+arturmelo18@users.noreply.github.com>
2026-09-06 16:44:01 -0300

    Update server/api/store/getStore.ts

--- server/api/store/getStore.ts
+++ server/api/store/getStore.ts
@@ -3,7 +3,7 @@ import { StoreSchema } from '~/server/models/store'
 export default defineEventHandler(async (event) => {
     const query = getQuery(event)
     const rawStore = String(query.store || '').trim()
-    const community = rawCommunity.replace(/^@/, '')
+    const store = rawStore.replace(/^@/, '')
 
     const filter = store ? { store } : {}
     const store = await StoreSchema.findOne(filter).lean()
